448547529f7da86f4521e5b435340561740ac92f
leeys <dmzld9403@naver.com>
2017-10-07 19:39:05 +0900

    no error in utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,34 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	*argv = (char**)malloc(sizeof(char)*100);//3-1=2
+	//char* token = malloc(sizeof(char)*100);
+	*argc = 0;
+	
+
+	char str[100];
+
+
+	strcpy(str, command);
+
+	char* token = strtok(str," ");
+
+	while(token != NULL)
+	{
+		(*argv)[*argc]=(char*)malloc(sizeof(char)*100);
+		strcpy((*argv)[*argc],token);
+		(*argc)++;
+		
+		token = strtok(NULL," ");
+	}
+
+	
+
 }
