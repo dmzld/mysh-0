16df530ea5d86ac9a4a8295a2afe09e542592af2
leeys <dmzld9403@naver.com>
2017-10-09 02:15:57 +0900

    8 of 13 tests success

--- src/commands.c
+++ src/commands.c
@@ -11,7 +11,7 @@
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
- 
+		
   char* buff=(char*)malloc(sizeof(char)*1024);
   int i=0; 
 
@@ -61,17 +61,16 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  /*if(argv[1]==NULL)
+  if(argc==1)
   	return 0;
-  */
+  
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  /*if(argv[1]!=NULL){
-  	printf("argv[1] = %s !!",argv[1]);
+  if(argc>1){
   	return 0;
-  }*/
+  }
 
   return 1;
 }
