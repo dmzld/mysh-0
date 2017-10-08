5da8641113189ae6985109c026a90ecfc65d926c
leeys <dmzld9403@naver.com>
2017-10-09 04:00:18 +0900

    to my repository

--- src/utils.c
+++ src/utils.c
@@ -13,31 +13,31 @@ void mysh_parse_command(const char* command,
 
 	//char str[1024];
 	char* str = (char*)malloc(sizeof(char)*1024);
-
 	strcpy(str, command);
 
-	
-
 	char* token = strtok(str," ");
+	
+	while(token != NULL){
 		
-	while(token != NULL)
-	{
 		(*argv)[*argc]=(char*)malloc(sizeof(char)*1024);//3-2=1
 		
 		strcpy((*argv)[*argc],token);
 		(*argc)++;
-
+		
 		token = strtok(NULL," ");
+		
 	}
-
-	(*argv)[*argc]=NULL;
+	
+	if((*argc)>=1){
+		(*argv)[*argc]=NULL;
 	//printf("argc-1 : %s\n",(*argv)[*argc-1]);
-	//printf("argc : %s\n",(*argv)[*argc]);
+	//printf("argv : %s\n",(*argv)[0]);
 	//(*argc)--;//  --> X 
 	//remove '\n'
-	char* last = strtok((*argv)[(*argc)-1],"\n");
-	strncpy((*argv)[(*argc)-1],last,sizeof(last)-1);
-	
+		char* last = strtok((*argv)[(*argc)-1],"\n");
+		strncpy((*argv)[(*argc)-1],last,sizeof(last)-1);
+	}
+
 	free(str);
 	
 }
