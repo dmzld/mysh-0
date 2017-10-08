0695b8658e5e74d02ddfb1e0ef1f73317d014f00
leeys <dmzld9403@naver.com>
2017-10-08 19:01:09 +0900

    modify validate & do fuction

--- src/commands.c
+++ src/commands.c
@@ -9,12 +9,32 @@
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
+ 
+  char buff[1024];
+  int i=0;
 
+  if(strcmp(".",argv[1])){
+  	return 0;
+  }
+  else if(strcmp("..",argv[1])){
+  	getcwd(buff,1024);
+	// tokenize at last / 
+  }
+  else if(strcmp("/",argv[1])){
+  	chdir("/");
+  }
+  else if(strcmp("~",argv[1])){
+  	chdir("/home");
+  }
+  else{
+  chdir(argv[1]);// if command cd ~ -> cd in [0], all of ~ in [1]
+  }
   return 0;
 }
 
+//0 == fales --> !0 == true
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
+  if (!validate_pwd_argv(argc, argv))//in 1 -> pass
     return -1;
 	
   char buff[1024];
@@ -26,17 +46,20 @@ int do_pwd(int argc, char** argv) {
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  char* str1;
-  strcpy(str1,*argv);
-  char* str2 = "cd";
-  if(!strcmp(str1,str2)) return 0;
-  return 1;
+  int i;
+
+  for(i=0; i==argc; i++){
+  	if(sizeof(argv[i])!=0){
+		return 0;
+	}
+  }
+  return 1;//correct
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  char* str1;
-  strcpy(str1,*argv);
-  char* str2 = "pwd";
-  if(!strcmp(str1,str2)) return 0;
+  if(argv[1]!=NULL){
+  	return 0;
+  }
+  
   return 1;
 }
