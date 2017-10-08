87d01a6aca488c7dc0b7871e0b395c89682c4689
leeys <dmzld9403@naver.com>
2017-10-09 02:02:07 +0900

    validate,2parsing test incomplete

--- src/commands.c
+++ src/commands.c
@@ -2,29 +2,43 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include "commands.h"
 
 
-// !1 == 0  == true
+// !0 == 1  == true
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
  
-  char buff[1024];
-  int i=0;
+  char* buff=(char*)malloc(sizeof(char)*1024);
+  int i=0; 
 
-  if(strcmp(".",argv[1])){
+  if(!strcmp(".",argv[1])){
   	return 0;
   }
-  else if(strcmp("..",argv[1])){
-  	getcwd(buff,1024);
-	// tokenize at last / 
+  else if(!strcmp("..",argv[1])){
+  		
+	if(!strcmp(buff,"/home")){
+	chdir("/");
+	}
+	else{
+	getcwd(buff,1024);
+	char* upper = strrchr(buff,'/');
+	*upper = '\0';
+	chdir(buff);
+	}
+
   }
-  else if(strcmp("/",argv[1])){
+  else if(!strcmp("/",argv[1])){
   	chdir("/");
   }
-  else if(strcmp("~",argv[1])){
-  	chdir("/home");
+  else if(!strcmp("~",argv[1])){
+  	chdir("/home/aeis");
+  }
+  else if(argv[1]==NULL){
+    return 0;
   }
   else{
   chdir(argv[1]);// if command cd ~ -> cd in [0], all of ~ in [1]
@@ -37,29 +51,27 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))//in 1 -> pass
     return -1;
 	
-  char buff[1024];
+  char* buff=(char*)malloc(sizeof(char)*1024);
   getcwd(buff,1024);
   printf("%s\n",buff);
  	
+  free(buff);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  int i;
-
-  for(i=0; i==argc; i++){
-  	if(sizeof(argv[i])!=0){
-		return 0;
-	}
-  }
-  return 1;//correct
+  /*if(argv[1]==NULL)
+  	return 0;
+  */
+  return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  if(argv[1]!=NULL){
+  /*if(argv[1]!=NULL){
+  	printf("argv[1] = %s !!",argv[1]);
   	return 0;
-  }
-  
+  }*/
+
   return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -11,27 +11,33 @@ void mysh_parse_command(const char* command,
 	*argc = 0;
 	
 
-	char str[1024];
-	
+	//char str[1024];
+	char* str = (char*)malloc(sizeof(char)*1024);
 
 	strcpy(str, command);
 
-	char* token = strtok(str," ");
+	
 
+	char* token = strtok(str," ");
+		
 	while(token != NULL)
 	{
 		(*argv)[*argc]=(char*)malloc(sizeof(char)*1024);//3-2=1
 		
 		strcpy((*argv)[*argc],token);
 		(*argc)++;
-		
+
 		token = strtok(NULL," ");
 	}
-	
-	//(*argc)--;  --> why make fail?
+
+	(*argv)[*argc]=NULL;
+	//printf("argc-1 : %s\n",(*argv)[*argc-1]);
+	//printf("argc : %s\n",(*argv)[*argc]);
+	//(*argc)--;//  --> X 
 	//remove '\n'
 	char* last = strtok((*argv)[(*argc)-1],"\n");
 	strncpy((*argv)[(*argc)-1],last,sizeof(last)-1);
 	
-
+	free(str);
+	
 }
